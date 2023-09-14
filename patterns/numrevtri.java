8ce9b57ecaa9d7656b5c2a7b7b7f83fb69fe31a8
gangapurambhargav <gangapurambhargav1@gmail.com>
2023-09-14 19:10:46 +0530

    pattern problems

--- patterns/numrevtri.java
+++ patterns/numrevtri.java
@@ -1,12 +1,12 @@
 package patterns;
 
-public class tripattern {
+public class numrevtri {
     public static void main(String[] args)
     {
         int n=10;
-        for(int i =1; i<=;i++){
-            for(int j=1; j<=i;j++){
-                System.out.print("*");
+        for(int i =1; i<=n;i++){
+            for(int j=1; j<=n-i+1;j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
--- patterns/revtriangle.java
+++ patterns/revtriangle.java
@@ -1,15 +1,15 @@
 package patterns;
 
-public class tripattern {
-    public static void main(String[] args)
+public class revtriangle {
+    public static void  main(String[] args)
     {
         int n=10;
-        for(int i =1; i<=;i++){
+        for(int i =n; i>=1;i--){
             for(int j=1; j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    
 }
+
