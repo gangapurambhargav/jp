efea6d1b5c1b7550acd61ecc964624b4846a7729
gangapurambhargav <gangapurambhargav1@gmail.com>
2023-09-14 18:23:19 +0530

    pattern problems

---
+++ patterns/patternrect.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class patternrect{
+    public static void main(String[] args){
+
+    
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=50;j++){
+            System.out.print("*");
+        } // for loop for j 
+      System.out.println();
+    } //for i loop
+    
+}
+ }
